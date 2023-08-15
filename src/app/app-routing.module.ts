a46f74a90b61715fc255d9425f83752fd0331ce0
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-08-14 22:00:59 -0300

    Added component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'main', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

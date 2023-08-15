c8a105843ff562c7a03b3ba8611170b1087fc455
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-08-14 22:00:28 -0300

    added declaration of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
